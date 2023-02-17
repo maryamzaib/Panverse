e52d194f95638f0ce263ba83480fa89b9e9b2861
maryamzaib <125693314+maryamzaib@users.noreply.github.com>
2023-02-18 00:45:25 +0500

    Update Q10.ts

--- Q10/Q10.ts
+++ Q10/Q10.ts
@@ -13,8 +13,6 @@ console.log("Stripped name: " + strippedName);
 
 
 // Program 2 
-//Muhammad Ansab Sultan
-//15 feb, 2023
 //This program prints operations of addition, subtration, multiplication and division
 console.log(4+4);
 console.log(12-4);
